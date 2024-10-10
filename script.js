9531fa3d58210e55e7cf8115560babed69735b0c
Kelly J <[kelly4410@gmail.com]>
2024-10-10 14:30:32 -0600

    step 5 in process

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM CONTENT LOADED');
 
 
+
     //2 Get the elements from the DOM that you will work on(box 
     // container, new box button, color form, color input).
 
@@ -26,9 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     colorForm.addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission behavior
+        console.log(colorInput.value);
         const colorValue = colorInput.value; // Get the value from the input field
         const boxes = document.getElementsByClassName('box'); // Get all elements with the class 'box'
-
         for (let i = 0; i < boxes.length; i++) {
             boxes[i].style.backgroundColor = colorValue;   // Loop through all boxes and set their background color
         }
@@ -38,5 +39,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
         colorInput.value = ''; // Reset the input field
     });
-})
 
+
+
+
+
+
+
+    //5 Create a function that adds a new box. 
+
+    //In this function, set the box ID as content, the class name, ????
+    //and the background color from the box color variable we created in Step 3.
+
+    // Besides, set the box ID to a data attribute. -??????
+
+    //You'll need this attribute to set the display text back to ID when the mouse leaves. (inner text????)
+    //Since we used this counter ID, increment it using the counter variable 
+    //we created in Step 3 to keep its uniqueness.
+
+    const createNewBox = function () {
+        const box = document.createElement('div'); //creates the box
+
+        box.setAttribute ?????
+
+            box.className = "box"; //create css class box
+        box.style.backgroundColor = boxColor; //sets the background color to the box color variable
+        boxContainer.appendChild(box); //appends box to the box container div when you create a new box
+
+        boxIdCounter++; //increment the box ID counter
+    }
+
+
+    //10 To create a new box when the N key is pressed, listen to the key-down events
+    // in the document. If the key is N (check both upper and lower cases), call
+    // the function that adds a new box. Remember to ignore the event if it is
+    // triggered from the color input element.
+
+    document.addEventListener('keypress', function (e) {
+        e.preventDefault();
+        if (e.key === "N" || e.key === "n") {
+            createNewBox();
+        }
+    })
+
+})
