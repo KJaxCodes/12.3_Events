df0cf9367e4117a750dbe47d304acdb885811248
Kelly J <[kelly4410@gmail.com]>
2024-10-13 14:52:38 -0600

    updates

--- script.js
+++ script.js
@@ -20,6 +20,29 @@ document.addEventListener('DOMContentLoaded', function () {
     let boxColor = null; // Stores the selected box color from the form.
     let boxIdCounter = 0; // Counter for assigning unique IDs to new boxes.
 
+    //5 Create a function that adds a new box. 
+
+    //In this function, set the box ID as content, the class name, ????
+    //and the background color from the box color variable we created in Step 3.
+
+    // Besides, set the box ID to a data attribute. -??????
+
+    //You'll need this attribute to set the display text back to ID when the mouse leaves. (inner text????)
+    //Since we used this counter ID, increment it using the counter variable 
+    //we created in Step 3 to keep its uniqueness.
+    const createNewBox = function () {
+        const box = document.createElement('div'); //creates the box
+        console.dir(box);
+        box.className = "box"; //create css class box
+        box.style.backgroundColor = boxColor; //sets the background color to the box color variable
+        boxContainer.appendChild(box); //appends box to the box container div when you create a new box
+
+        boxIdCounter++; //increment the box ID counter
+        box.setAttribute('id', `box-${boxIdCounter}`);
+        box.textContent = `Box ${boxIdCounter}`; // Display the ID inside the box
+
+    }
+
     //4 When the form is submitted, get the value from the color input element 
     // from Step 2 and set this color to all boxes (to get all boxes, use the 
     // class name box). Remember to reset the color input element's value and 
@@ -40,46 +63,41 @@ document.addEventListener('DOMContentLoaded', function () {
         colorInput.value = ''; // Reset the input field
     });
 
+    newBoxBtn.addEventListener('click', function () {
+        createNewBox();
+    })
 
 
+    boxContainer.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        const box = document.querySelector('.box');
+        box.remove();
+    })
 
+    boxContainer.addEventListener('mouseover', function (event) {
+        if (event.target.classList.contains("box")) {
+            event.target.textContent = `x: ${event.pageX}, y: ${event.pageY}`; //Changes the display text to the coordinates.
+        }
+    })
 
-
-
-    //5 Create a function that adds a new box. 
-
-    //In this function, set the box ID as content, the class name, ????
-    //and the background color from the box color variable we created in Step 3.
-
-    // Besides, set the box ID to a data attribute. -??????
-
-    //You'll need this attribute to set the display text back to ID when the mouse leaves. (inner text????)
-    //Since we used this counter ID, increment it using the counter variable 
-    //we created in Step 3 to keep its uniqueness.
-
-    const createNewBox = function () {
-        const box = document.createElement('div'); //creates the box
-
-        box.setAttribute ?????
-
-            box.className = "box"; //create css class box
-        box.style.backgroundColor = boxColor; //sets the background color to the box color variable
-        boxContainer.appendChild(box); //appends box to the box container div when you create a new box
-
-        boxIdCounter++; //increment the box ID counter
-    }
-
+    boxContainer.addEventListener('mouseout', function (event) {
+        if (event.target.classList.contains("box")) {
+            event.target.textContent = `Box ${boxIdCounter}`; // Return to displaying the ID inside the box after mouse moves out of box.
+        }
+    })
 
     //10 To create a new box when the N key is pressed, listen to the key-down events
     // in the document. If the key is N (check both upper and lower cases), call
     // the function that adds a new box. Remember to ignore the event if it is
     // triggered from the color input element.
 
-    document.addEventListener('keypress', function (e) {
-        e.preventDefault();
-        if (e.key === "N" || e.key === "n") {
-            createNewBox();
-        }
-    })
+    // document.addEventListener('keypress', function (e) {
+    //     e.preventDefault();
+    //     //if(write a conditional to say, don't do anything/ignore if keys pressed in the form input box, target???)
+    //     else (e.key === "N" || e.key === "n") {
+    //         createNewBox();
+    //     }
+    // })
+
 
 })
